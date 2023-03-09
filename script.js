04186f5f546a039bb1744e3d1e2e15730afd6429
Piyaporn <piyapornlahr@gmail.com>
2023-03-09 13:14:21 +0000

    Challenge-1 (isNumberPositive) completed

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 // 1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
 // isNumberPositive(-1); // returns false
 // isNumberPositive(10); // returns true
+const isNumberPositive = (getNumber) => {
+    if (getNumber >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(10));
 
 // 2. Write a function that takes a number of days and converts it into an age.
 // convertDaysToAge(3650); // returns 10
